10d1c1c63bdf0063cdcfd3a0cb4b6c8c0de0dd47
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-04-09 21:41:57 +0900

    DRY CODE

--- lib/pekostream/stream/github.rb
+++ lib/pekostream/stream/github.rb
@@ -17,52 +17,56 @@ module Pekostream
       def run
         @client.received_events('ryopeko').each do |event|
           created_at = event.created_at.in_time_zone('Asia/Tokyo')
+
           if created_at < @last_checked_at
             output('skipped..')
             break
           end
 
-          payload = event.payload
-          actor = event.actor.login.colorlize
+          payload   = event.payload
+          actor     = event.actor.login.colorlize
+          repo_name = event.repo.name
 
           case event.type.to_sym
           when :WatchEvent
-            output "#{actor} starred #{event.repo.name} at #{created_at}"
+            output "#{actor} starred #{repo_name} at #{created_at}"
           when :ForkEvent
-            output "#{actor} forked #{event.repo.name} at #{created_at}"
+            output "#{actor} forked #{repo_name} at #{created_at}"
           when :CreateEvent
             case payload.ref_type.to_sym
             when :repository
-              output "#{actor} created repository #{event.repo.name} at #{created_at}"
+              output "#{actor} created repository #{repo_name} at #{created_at}"
             else
               output payload.ref_type
               pp event
             end
           when :IssuesEvent
-            output "#{actor} #{payload.action} issue #{event.repo.name}##{payload.issue.number}"
+            output "#{actor} #{payload.action} issue #{repo_name}##{payload.issue.number}"
           when :IssueCommentEvent
-            output "#{actor} commented on issue #{event.repo.name}##{payload.issue.number}"
+            output "#{actor} commented on issue #{repo_name}##{payload.issue.number}"
           when :GistEvent
+            gist_id = payload.gist.id
+
             case payload.action.to_sym
             when :create
-              output "#{actor} created gist #{payload.gist.id} at #{created_at}"
+              output "#{actor} created gist #{gist_id} at #{created_at}"
             when :update
-              output "#{actor} updated gist #{payload.gist.id} at #{created_at}"
+              output "#{actor} updated gist #{gist_id} at #{created_at}"
             else
               output payload.action
               pp event
             end
           when :MemberEvent
-            output "#{actor} added #{payload.member.login} to #{event.repo.name} at #{created_at}"
+            output "#{actor} added #{payload.member.login} to #{repo_name} at #{created_at}"
           when :PublicEvent
-            output "#{actor} open sourced #{event.repo.name} at #{created_at}"
+            output "#{actor} open sourced #{repo_name} at #{created_at}"
           when :PushEvent
             branch_name = payload.ref.match(/refs\/heads\/(.+)$/)[1]
-            output "#{actor} pushed to #{branch_name} at #{event.repo.name} #{created_at}"
+            output "#{actor} pushed to #{branch_name} at #{repo_name} #{created_at}"
           when :PullRequestEvent
-            output "#{actor} #{payload.action} #{event.repo.name}##{payload.pull_request.number} #{created_at}"
+            output "#{actor} #{payload.action} #{repo_name}##{payload.pull_request.number} #{created_at}"
           when :DeleteEvent
-            output "#{actor} deleted #{payload.ref_type} #{payload.ref} #{event.repo.name} at #{created_at}"
+            output "#{actor} deleted #{payload.ref_type} #{payload.ref} #{repo_name} at #{created_at}"
           else
             output event.type
             pp event
