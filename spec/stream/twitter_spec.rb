aa6c9d0c10d90d84499c85c6d1b7a5b14f92665e
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-04-25 22:53:43 +0900

    add invoke test

--- spec/stream/twitter_spec.rb
+++ spec/stream/twitter_spec.rb
@@ -16,24 +16,51 @@ describe Pekostream::Stream::Twitter do
   }
 
   describe "#favorite" do
-    it 'should call #invoke' do
-      expect(subject).to receive(:invoke)
-      subject.favorite(::Twitter::Streaming::Event.new(
-        event: 'favorite',
-        source: {
-          id: '2311766360',
-          screen_name: 'ryopeko_test'
-        },
-        target: {
-          id: '15562743',
-          screen_name: 'ryopeko'
-        },
-        target_object: {
-          id: 459259871234244608,
-          text: 'hogehoge',
-          screen_name: 'ryopeko'
-        }
-      ))
+    before do
+      @me = 'ryopeko'
+      @target_object = {
+        id: 459259871234244608,
+        text: 'hogehoge',
+        screen_name: @me
+      }
+    end
+
+    context "when source event's user" do
+      context "is other user" do
+        it 'should call #invoke' do
+          expect(subject).to receive(:invoke)
+          subject.favorite(::Twitter::Streaming::Event.new(
+            event: 'favorite',
+            source: {
+              id: '2311766360',
+              screen_name: 'ryopeko_test'
+            },
+            target: {
+              id: '15562743',
+              screen_name: @me
+            },
+            target_object: @target_object
+          ))
+        end
+      end
+
+      context "is me" do
+        it 'should not call #invoke' do
+          expect(subject).to receive(:invoke).exactly(0).times
+          subject.favorite(::Twitter::Streaming::Event.new(
+            event: 'favorite',
+            source: {
+              id: '2311766360',
+              screen_name: @me
+            },
+            target: {
+              id: '15562743',
+              screen_name: @me
+            },
+            target_object: @target_object
+          ))
+        end
+      end
     end
   end
 end
