7144fba4b4868b26ace7465bfd366328776f86e8
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-04-26 23:21:45 +0900

    add spec for #tweet

--- spec/stream/twitter_spec.rb
+++ spec/stream/twitter_spec.rb
@@ -2,6 +2,10 @@ require 'pekostream/stream/twitter'
 require 'twitter'
 
 describe Pekostream::Stream::Twitter do
+  before do
+    @me = 'ryopeko'
+  end
+
   subject {
     described_class.new do |config|
       config.screen_name = 'ryopeko'
@@ -17,7 +21,6 @@ describe Pekostream::Stream::Twitter do
 
   describe "#favorite" do
     before do
-      @me = 'ryopeko'
       @target_object = {
         id: 459259871234244608,
         text: 'hogehoge',
@@ -60,6 +63,39 @@ describe Pekostream::Stream::Twitter do
             target_object: @target_object
           ))
         end
+      end
+    end
+  end
+
+  describe "#tweet" do
+    before do
+      @tweet_data = {
+        :created_at=>"Sat Apr 26 13:48:14 +0000 2014",
+        :id=>460052735073611776,
+        :id_str=>"460052735073611776",
+        :text=>"提督各位大変そう...",
+        :source=>"<a href=\"http://sites.google.com/site/yorufukurou/\" rel=\"nofollow\">YoruFukurou</a>",
+        :user=> {
+          :id=>15562743,
+          :id_str=>"15562743",
+          :name=>"新卒ふなっしー",
+          :screen_name=>"ryopeko",
+        },
+        :favorited=>false,
+        :retweeted=>false
+      }
+    end
+    context "when normal text" do
+      it "should not call #invoke" do
+        expect(subject).to receive(:invoke).exactly(0).times
+        subject.tweet(::Twitter::Tweet.new(@tweet_data))
+      end
+    end
+
+    context "when text included of RT" do
+      it "should call #invoke" do
+        expect(subject).to receive(:invoke)
+        subject.tweet(::Twitter::Tweet.new(@tweet_data.merge(:text=>"RT @ryopeko_retro: hogehoge")))
       end
     end
   end
