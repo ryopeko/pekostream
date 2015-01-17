2c71d3281742f4aed5c34082e75dbca89793e64f
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2015-01-17 22:30:18 +0900

    Add event

--- lib/pekostream/cli.rb
+++ lib/pekostream/cli.rb
@@ -39,7 +39,7 @@ module Pekostream
       github_stream = Pekostream::Stream::Github.new do |config|
         config.username     = @config.github['username']
         config.access_token = @config.github['access_token']
-        config.notify_event_types = [ :WatchEvent ]
+        config.notify_event_types = [ :WatchEvent, :PushEvent ]
         config.event(:notify, method(:notify))
       end
       github_stream.start
